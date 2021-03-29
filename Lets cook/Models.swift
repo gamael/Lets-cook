1f4a8a66f9652a77c555891c227c96ac55180b3c
Alejandro <carlosaguirre7@hotmail.com>
2021-03-29 01:43:47 -0500

    Feat: image and rating implemented

--- Lets cook/Models.swift
+++ Lets cook/Models.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 enum RequestResult<T> {
     case success(T)
@@ -39,4 +40,32 @@ extension Encodable {
     return try encoder.encode(self)
   }
   
+}
+
+extension UIImageView {
+    func loadImage(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        let activityView = UIActivityIndicatorView(style: .medium)
+        activityView.center = center
+        addSubview(activityView)
+        activityView.startAnimating()
+        activityView.hidesWhenStopped = true
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+            DispatchQueue.main.async {
+                activityView.stopAnimating()
+            }
+        }
+    }
+    
+    func loadImage(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        loadImage(from: url, contentMode: mode)
+    }
 }
--- RecipeDetailViewController.swift
+++ RecipeDetailViewController.swift
@@ -13,9 +13,15 @@ class RecipeDetailViewController: UIViewController {
     @IBOutlet weak var recipeTitle: UILabel!
     @IBOutlet weak var recipeTimage: UIImageView!
     @IBOutlet weak var recipeDetails: UILabel!
+    @IBOutlet weak var starsStackView: UIStackView!
     
     let presenter = RecipeDetailPresenter()
     var recipeId: Int = 0
+    
+    struct Constants {
+        static let filledStarImage = UIImage(named: "fill_star")
+        static let unfilledStarImage = UIImage(named: "unfilled_star")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,5 +36,19 @@ class RecipeDetailViewController: UIViewController {
     func updateViewWith(recipe: RecipeDetail) {
         recipeTitle.text = recipe.title
         recipeDetails.text = recipe.instructions
+        recipeTimage.loadImage(from: recipe.image)
+        addRating(recipe.rating)
+    }
+    
+    private func addRating(_ rating: Int) {
+        for i in 1...5 {
+            if rating >= i {
+                let imageView = UIImageView(image: Constants.filledStarImage)
+                starsStackView.addArrangedSubview(imageView)
+            } else {
+                let imageView = UIImageView(image: Constants.unfilledStarImage)
+                starsStackView.addArrangedSubview(imageView)
+            }
+        }
     }
 }
