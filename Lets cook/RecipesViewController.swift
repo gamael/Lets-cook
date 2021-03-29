517659735d9ff812e56ef010f20e6832821106cb
Alejandro <carlosaguirre7@hotmail.com>
2021-03-28 22:16:26 -0500

    Feat: UI created

---
+++ Lets cook/RecipesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipesViewController.swift
+//  Lets cook
+//
+//  Created by Alejandro Agudelo on 28/03/21.
+//  Copyright © 2021 Alejandro Agudelo. All rights reserved.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
